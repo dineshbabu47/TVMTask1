29ceba3fe259e06164e9aad5b9f969ed0249bb54
DINESH BABUM M <DINESH BABUM M@DESKTOP-HLFT0U2>
2021-04-25 17:20:04 +0530

    update commit

--- TVMTask-1/src/main/java/com/java/task/ExampleGitup.java
+++ TVMTask-1/src/main/java/com/java/task/ExampleGitup.java
@@ -4,7 +4,7 @@ public class ExampleGitup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hii gitup");
+System.out.println("hii gitup"); System.out.println("hii gitup");
 	}
 
 }
